e17634f3ce2c6f77ed3f276de0fc569722957ee4
Michal Papis <mpapis@gmail.com>
2016-09-08 20:42:08 +0200

    move stats data to controler

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
   def index
+    @user_count = User.count
+    @club_count = Club.count
+    @confirmed_clubs_count = Club.with_confirmed_state.count
   end
 end
