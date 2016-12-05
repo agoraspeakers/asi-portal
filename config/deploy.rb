d1d89d4bdbce6181354f2d444f928a62980a81e4
Michal Papis <mpapis@gmail.com>
2016-12-05 14:10:33 +0100

    fix bundlers gems installation in capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,12 +9,16 @@ set :migration_role, :app
 set :conditionally_migrate, true
 
 # automatic linking configuration
-set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
+set :linked_dirs,  %w{log tmp/pids tmp/cache tmp/sockets public/system public/uploads}
 set :linked_files, %w{.env}
 
 # Default branch is :master
 branch = `git rev-parse --abbrev-ref HEAD`.chomp
 ask :branch, branch if branch != "master"
+
+# bundler by default uses it's own gems path, let's use GEM_HOME/PATH:
+set :bundle_flags, '--quiet'
+set :bundle_path, nil
 
 # Default value for :pty is false
 # set :pty, true
